8d32f7862f1740b47a83e707f5b8f108f1f19979
Markus Lindenberg <markus@lindenberg.io>
2018-10-23 18:46:24 +0200

    Use systemd-resolved resolver

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -6,3 +6,6 @@ set -e
 # Disable systemd-journald persistent storage
 rmdir ${TARGET_DIR}/var/log/journal
 
+# Enable resolved stub resolver
+ln -sf ../run/systemd/resolve/stub-resolv.conf ${TARGET_DIR}/etc/resolv.conf
+
