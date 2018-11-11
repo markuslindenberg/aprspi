5aa98c18fb2da39ccd9e0d3a8c02a0e85b8c1854
Markus Lindenberg <markus@lindenberg.io>
2018-11-11 14:14:54 +0100

    Add i2c/spi/gpio groups and udev rules

--- package/direwolf/direwolf.mk
+++ package/direwolf/direwolf.mk
@@ -9,4 +9,8 @@ DIREWOLF_SITE = https://github.com/wb2osz/direwolf.git
 DIREWOLF_SITE_METHOD = git
 DIREWOLF_DEPENDENCIES = alsa-lib gpsd hamlib udev host-gen_fff
 
+define DIREWOLF_USERS
+	direwolf -1 direwolf -1 * - - audio,gpio Direwolf TNC
+endef
+
 $(eval $(cmake-package))
