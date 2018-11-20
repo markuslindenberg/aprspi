72216f95e297a3fc95f8e20be678950cb64a4c7f
Markus Lindenberg <markus@lindenberg.io>
2018-11-20 19:46:28 +0100

    Build iwd with internal ell, use latest iwd/ell versions

--- package/iwd/iwd.mk
+++ package/iwd/iwd.mk
@@ -4,17 +4,26 @@
 #
 ################################################################################
 
-IWD_VERSION = 0.10
+IWD_VERSION = 0.12
 IWD_SITE = https://git.kernel.org/pub/scm/network/wireless/iwd.git
 IWD_SITE_METHOD = git
-IWD_CONF_OPTS = --enable-external-ell
-IWD_DEPENDENCIES = readline ell
+IWD_DEPENDENCIES = readline
 IWD_AUTORECONF = YES
+
+define IWD_PRE_CONFIGURE_ELL
+	rm -rf $(@D)/../ell
+	git clone -b 0.15 https://git.kernel.org/pub/scm/libs/ell/ell.git $(@D)/../ell
+endef
 
 define IWD_PRE_CONFIGURE_BOOTSTRAP
         mkdir -p $(@D)/build-aux
 endef
 
-IWD_PRE_CONFIGURE_HOOKS += IWD_PRE_CONFIGURE_BOOTSTRAP
+define IWD_PRE_BUILD_GENERATE_ELL
+	make -C $(@D) ell/internal ell/ell.h
+endef
+
+IWD_PRE_CONFIGURE_HOOKS += IWD_PRE_CONFIGURE_ELL IWD_PRE_CONFIGURE_BOOTSTRAP
+IWD_PRE_BUILD_HOOKS += IWD_PRE_BUILD_GENERATE_ELL
 
 $(eval $(autotools-package))
